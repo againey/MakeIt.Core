29603252ba7722f2905cd89e18c9c6b74aa29fba
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-03-03 21:37:38 -0600

    Copied the Index2D editor to make an Index3D editor also. Cleaned up the control placement code of the Index2D and Index3D editors.

---
+++ Assets/Plugins/Editor/Experilous/Index3DEditor.cs
@@ -0,0 +1,48 @@
+﻿/******************************************************************************\
+ *  Copyright (C) 2016 Experilous <againey@experilous.com>
+ *  
+ *  This file is subject to the terms and conditions defined in the file
+ *  'Assets/Plugins/Experilous/License.txt', which is a part of this package.
+ *
+\******************************************************************************/
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Experilous.Topological
+{
+	[CustomPropertyDrawer(typeof(Index3D))]
+	public class Index3DEditor : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+			float spacing = 2f;
+			float labelWidth = EditorStyles.label.CalcSize(new GUIContent("M")).x;
+			float fullFieldWidth = (position.width + spacing) / 3f;
+			float inputFieldWidth = fullFieldWidth - labelWidth - spacing;
+			
+			float offset = position.x;
+			GUI.Label(new Rect(offset, position.yMin, labelWidth, position.height), "X");
+			property.FindPropertyRelative("x").intValue = EditorGUI.IntField(
+				new Rect(offset + labelWidth, position.yMin, inputFieldWidth, position.height),
+				property.FindPropertyRelative("x").intValue);
+
+			offset = position.x + fullFieldWidth;
+			GUI.Label(new Rect(offset, position.yMin, labelWidth, position.height), "Y");
+			property.FindPropertyRelative("y").intValue = EditorGUI.IntField(
+				new Rect(offset + labelWidth, position.yMin, inputFieldWidth, position.height),
+				property.FindPropertyRelative("y").intValue);
+
+			offset = position.xMax - fullFieldWidth + spacing;
+			GUI.Label(new Rect(offset, position.yMin, labelWidth, position.height), "Z");
+			property.FindPropertyRelative("z").intValue = EditorGUI.IntField(
+				new Rect(offset + labelWidth, position.yMin, inputFieldWidth, position.height),
+				property.FindPropertyRelative("z").intValue);
+
+			EditorGUI.EndProperty();
+		}
+	}
+}
