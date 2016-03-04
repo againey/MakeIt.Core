26f5e50410512d2b4b8781c41157e9638b550203
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-03-03 18:39:20 -0600

    Added a texture demo. Reworked the dynamic mesh and supporting classes to be far more flexible with triangulation. Expanded the triangulation strategies to enable multiple layers of rings. Added an aspect ratio utility class, and plenty of functions for fitting 2D vectors to a specified aspect ratio according to a variety of strategies. Finished up the UV generation functions. Added an inner angle bisector calculation function, which is convenient for triangulating a face so that a border of uniform thickness can be rendered. Added generic min and range calculations to be applied to all the vertices or edges of a face, for any float or vector data type.  Currently used specifically for UV calculations. Added a thread utility class, which currently only tracks the main thread id so that it can report whether or not the current thread is the main thread.

---
+++ Assets/Plugins/Experilous/ThreadUtility.cs
@@ -0,0 +1,41 @@
+﻿/******************************************************************************\
+ *  Copyright (C) 2016 Experilous <againey@experilous.com>
+ *  
+ *  This file is subject to the terms and conditions defined in the file
+ *  'Assets/Plugins/Experilous/License.txt', which is a part of this package.
+ *
+\******************************************************************************/
+
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Threading;
+
+namespace Experilous
+{
+	public static class ThreadUtility
+	{
+		private static bool _mainThreadIdentified = false;
+		private static int _mainThreadId;
+
+#if UNITY_EDITOR
+		[InitializeOnLoadMethod]
+#endif
+		[RuntimeInitializeOnLoadMethod]
+		private static void OnLoad()
+		{
+			_mainThreadId = Thread.CurrentThread.ManagedThreadId;
+			_mainThreadIdentified = true;
+		}
+
+		public static bool isMainThread
+		{
+			get
+			{
+				return _mainThreadIdentified && Thread.CurrentThread.ManagedThreadId == _mainThreadId;
+			}
+		}
+	}
+}
