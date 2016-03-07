0a035b61cec332445016d2b15d7a2c01a63fd518
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-03-07 12:25:09 -0600

    Fixed an egregious bug in my base-2 logarithm functions, where the result was one smaller than it should be, but only for very specific input ranges.  Simplified the logic in the process, but at the expense of one additional arithmetic operator. Made the Next32() and Next64() functions pull from the upper bits instead of lower bits, when fewer than all bits are needed.  This ends up saving a bitwise operation too. Added a random seed to my random utility automated tests, to avoid testing everything from a potentially problematic starting seed of 0. Added some additional random utility automated tests. Added some base-2 logarithm automated tests.

---
+++ Assets/Editor/Tests/Experilous/MathUtilityTests.cs
@@ -0,0 +1,126 @@
+﻿/******************************************************************************\
+ *  Copyright (C) 2016 Experilous <againey@experilous.com>
+ *  
+ *  This file is subject to the terms and conditions defined in the file
+ *  'Assets/Plugins/Experilous/License.txt', which is a part of this package.
+ *
+\******************************************************************************/
+
+#if UNITY_5_3
+using UnityEngine;
+using NUnit.Framework;
+
+namespace Experilous.Tests
+{
+	public class MathUtilityTests
+	{
+		[Test]
+		public void Log2Ceil32BitPowerOfTwo()
+		{
+			for (int i = 0; i < 32; ++i)
+			{
+				Assert.AreEqual(i, MathUtility.Log2Ceil(1U << i), string.Format("MathUtility.Log2Ceil(1U << {0}), MathUtility.Log2Ceil({1})", i, 1U << i));
+			}
+		}
+
+		[Test]
+		public void Log2Ceil64BitPowerOfTwo()
+		{
+			for (int i = 0; i < 64; ++i)
+			{
+				Assert.AreEqual(i, MathUtility.Log2Ceil(1UL << i), string.Format("MathUtility.Log2Ceil(1UL << {0}), MathUtility.Log2Ceil({1})", i, 1UL << i));
+			}
+		}
+
+		[Test]
+		public void Plus1Log2Ceil32BitPowerOfTwo()
+		{
+			for (int i = 0; i < 32; ++i)
+			{
+				Assert.AreEqual(i + 1, MathUtility.Plus1Log2Ceil(1U << i), string.Format("MathUtility.Plus1Log2Ceil(1U << {0}), MathUtility.Plus1Log2Ceil({1})", i, 1U << i));
+			}
+		}
+
+		[Test]
+		public void Plus1Log2Ceil64BitPowerOfTwo()
+		{
+			for (int i = 0; i < 64; ++i)
+			{
+				Assert.AreEqual(i + 1, MathUtility.Plus1Log2Ceil(1UL << i), string.Format("MathUtility.Plus1Log2Ceil(1UL << {0}), MathUtility.Plus1Log2Ceil({1})", i, 1UL << i));
+			}
+		}
+
+		[Test]
+		public void Log2Ceil32BitPowerOfTwoMinusOne()
+		{
+			for (int i = 2; i < 32; ++i)
+			{
+				Assert.AreEqual(i, MathUtility.Log2Ceil((1U << i) - 1), string.Format("MathUtility.Log2Ceil((1U << {0}) - 1), MathUtility.Log2Ceil({1})", i, (1U << i) - 1));
+			}
+		}
+
+		[Test]
+		public void Log2Ceil64BitPowerOfTwoMinusOne()
+		{
+			for (int i = 2; i < 64; ++i)
+			{
+				Assert.AreEqual(i, MathUtility.Log2Ceil((1UL << i) - 1), string.Format("MathUtility.Log2Ceil((1UL << {0}) - 1), MathUtility.Log2Ceil({1})", i, (1UL << i) - 1));
+			}
+		}
+
+		[Test]
+		public void Plus1Log2Ceil32BitPowerOfTwoMinusOne()
+		{
+			for (int i = 2; i < 32; ++i)
+			{
+				Assert.AreEqual(i, MathUtility.Plus1Log2Ceil((1U << i) - 1), string.Format("MathUtility.Plus1Log2Ceil((1U << {0}) - 1), MathUtility.Plus1Log2Ceil({1})", i, (1U << i) - 1));
+			}
+		}
+
+		[Test]
+		public void Plus1Log2Ceil64BitPowerOfTwoMinusOne()
+		{
+			for (int i = 2; i < 64; ++i)
+			{
+				Assert.AreEqual(i, MathUtility.Plus1Log2Ceil((1UL << i) - 1), string.Format("MathUtility.Plus1Log2Ceil((1UL << {0}) - 1), MathUtility.Plus1Log2Ceil({1})", i, (1UL << i) - 1));
+			}
+		}
+
+		[Test]
+		public void Log2Ceil32BitPowerOfTwoPlusOne()
+		{
+			for (int i = 1; i < 32; ++i)
+			{
+				Assert.AreEqual(i + 1, MathUtility.Log2Ceil((1U << i) + 1), string.Format("MathUtility.Log2Ceil((1U << {0}) + 1), MathUtility.Log2Ceil({1})", i, (1U << i) + 1));
+			}
+		}
+
+		[Test]
+		public void Log2Ceil64BitPowerOfTwoPlusOne()
+		{
+			for (int i = 1; i < 64; ++i)
+			{
+				Assert.AreEqual(i + 1, MathUtility.Log2Ceil((1UL << i) + 1), string.Format("MathUtility.Log2Ceil((1UL << {0}) + 1), MathUtility.Log2Ceil({1})", i, (1UL << i) + 1));
+			}
+		}
+
+		[Test]
+		public void Plus1Log2Ceil32BitPowerOfTwoPlusOne()
+		{
+			for (int i = 1; i < 32; ++i)
+			{
+				Assert.AreEqual(i + 1, MathUtility.Plus1Log2Ceil((1U << i) + 1), string.Format("MathUtility.Plus1Log2Ceil((1U << {0}) + 1), MathUtility.Plus1Log2Ceil({1})", i, (1U << i) + 1));
+			}
+		}
+
+		[Test]
+		public void Plus1Log2Ceil64BitPowerOfTwoPlusOne()
+		{
+			for (int i = 1; i < 64; ++i)
+			{
+				Assert.AreEqual(i + 1, MathUtility.Plus1Log2Ceil((1UL << i) + 1), string.Format("MathUtility.Plus1Log2Ceil((1UL << {0}) + 1), MathUtility.Plus1Log2Ceil({1})", i, (1UL << i) + 1));
+			}
+		}
+	}
+}
+#endif
--- Assets/Plugins/Experilous/MathUtility.cs
+++ Assets/Plugins/Experilous/MathUtility.cs
@@ -37,27 +37,7 @@ namespace Experilous
 
 		#region Integer Base-2 Logarithms
 
-		private static sbyte[] _log2CeilLookupTable = // Table[i] = Ceil(Log2(i))
-		{
-			0, 0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4,
-			4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-			5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-			6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
-			6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-			7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-			7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-			8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-		};
-
-		private static sbyte[] _plus1Log2CeilLookupTable = // Table[i] = Ceil(Log2(i+1))
+		private static sbyte[] _log2CeilLookupTable = // Table[i] = Ceil(Log2(i+1))
 		{
 			0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4,
 			5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
@@ -88,21 +68,11 @@ namespace Experilous
 		/// </remarks>
 		public static int Log2Ceil(uint n)
 		{
-			var high16 = n >> 16;
-			if (high16 != 0)
-			{
-				var high8 = high16 >> 8;
-				return (high8 != 0) ? 24 + _log2CeilLookupTable[high8] : 16 + _log2CeilLookupTable[high16];
-			}
-			else
-			{
-				var high8 = n >> 8;
-				return (high8 != 0) ? 8 + _log2CeilLookupTable[high8] : _log2CeilLookupTable[n];
-			}
+			return Plus1Log2Ceil(n - 1);
 		}
 
 		/// <summary>
-		/// Calculates the integer base-2 logarithm of a supplied integer.
+		/// Calculates the integer base-2 logarithm of one greater than a supplied integer.
 		/// </summary>
 		/// <param name="n">The integer whose base-2 logarithm is to be calculated.</param>
 		/// <returns>The base-2 logarithm, rounded up to the nearest integer, of <paramref name="n"/>.</returns>
@@ -113,15 +83,15 @@ namespace Experilous
 		public static int Plus1Log2Ceil(uint n)
 		{
 			var high16 = n >> 16;
-			if (high16 != 0)
+			if (high16 != 0U)
 			{
 				var high8 = high16 >> 8;
-				return (high8 != 0) ? 24 + _plus1Log2CeilLookupTable[high8] : 16 + _plus1Log2CeilLookupTable[high16];
+				return (high8 != 0U) ? 24 + _log2CeilLookupTable[high8] : 16 + _log2CeilLookupTable[high16];
 			}
 			else
 			{
 				var high8 = n >> 8;
-				return (high8 != 0) ? 8 + _plus1Log2CeilLookupTable[high8] : _plus1Log2CeilLookupTable[n];
+				return (high8 != 0U) ? 8 + _log2CeilLookupTable[high8] : _log2CeilLookupTable[n];
 			}
 		}
 
@@ -136,15 +106,7 @@ namespace Experilous
 		/// </remarks>
 		public static int Log2Ceil(ulong n)
 		{
-			var high32 = n >> 32;
-			if (high32 != 0)
-			{
-				return 32 + Log2Ceil((uint)high32);
-			}
-			else
-			{
-				return Log2Ceil((uint)n);
-			}
+			return Plus1Log2Ceil(n - 1);
 		}
 
 		/// <summary>
@@ -159,7 +121,7 @@ namespace Experilous
 		public static int Plus1Log2Ceil(ulong n)
 		{
 			var high32 = n >> 32;
-			if (high32 != 0)
+			if (high32 != 0UL)
 			{
 				return 32 + Plus1Log2Ceil((uint)high32);
 			}
