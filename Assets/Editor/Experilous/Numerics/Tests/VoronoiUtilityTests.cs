f4efe44027719d5500e85cc6778b333a8109b80d
Andy Gainey <7810015+againey@users.noreply.github.com>
2017-03-10 13:28:39 -0600

    Added voronoi utility tests class.

---
+++ Assets/Editor/Experilous/Numerics/Tests/VoronoiUtilityTests.cs
@@ -0,0 +1,14 @@
+﻿/******************************************************************************\
+* Copyright Andy Gainey                                                        *
+\******************************************************************************/
+
+#if UNITY_5_3_OR_NEWER
+using NUnit.Framework;
+
+namespace Experilous.Numerics.Tests
+{
+	class VoronoiUtilityTests
+	{
+	}
+}
+#endif
